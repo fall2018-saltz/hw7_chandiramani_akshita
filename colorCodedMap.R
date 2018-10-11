9247a0136829a049acba18a17246ba35ea66217a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 12:03:41 -0400

    Saving colorCodedMap.R

--- colorCodedMap.R
+++ colorCodedMap.R
@@ -5,4 +5,4 @@ library("ggmap")
 
 usa <- map_data("usa") # we already did this, but we can do it again
 colormap <- ggplot() + geom_polygon(data = mergedData, 
-aes(x=long, y = lat, group = group, fill=StateArea)) + coord_fixed(1.3)
+aes(x=StateCenterX, y = StateCenterY, group = group, fill=StateArea)) + coord_fixed(1.3)
