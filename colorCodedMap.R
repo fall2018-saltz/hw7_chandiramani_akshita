d09ae591ced67d17bd06c05a2de18f78c7cb2cc9
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 12:10:48 -0400

    Saving colorCodedMap.R

--- colorCodedMap.R
+++ colorCodedMap.R
@@ -5,5 +5,4 @@ library("ggmap")
 
 usa <- map_data("usa") # we already did this, but we can do it again
 colormap <- ggplot(mergedData, aes(map_id = usa)) +
-    geom_map(map = usa) +
-    expand_limits(x = mergedData$StateCenterX, y = mergedData$StateCenterY)
+    geom_map(map = usa)
