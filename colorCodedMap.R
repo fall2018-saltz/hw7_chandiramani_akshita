7baf82ee048c83f4264be6aee9be35d7778c0709
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 12:02:40 -0400

    Saving colorCodedMap.R

--- colorCodedMap.R
+++ colorCodedMap.R
@@ -1,7 +1,6 @@
 
 library("ggplot2")
 library("ggmap")
-
 usa <- map_data("usa") # we already did this, but we can do it again
 colormap <- ggplot() + geom_polygon(data = usa, 
-aes(x=long, y = lat, group = group)) + coord_fixed(1.3)
+aes(x=long, y = lat, group = group, fill)) + coord_fixed(1.3)
