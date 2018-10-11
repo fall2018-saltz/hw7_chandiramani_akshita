0fcd01c9c93ca36a81cb907965d31aa4a1ac7676
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 11:35:49 -0400

    Saving merge.R

--- merge.R
+++ merge.R
@@ -1,1 +1,10 @@
 
+
+#3) Merge arrests and US data based on row names
+mergedData <- merge(arrests, cleanDfStates, by="row.names", all=TRUE)
+
+#Remove additional States column from merged table
+#and rename to StateName
+mergedData <- mergedData[,-c(6)]
+colnames(mergedData)[1] <- "StateName"
+
