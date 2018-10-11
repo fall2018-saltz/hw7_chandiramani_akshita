b339ad5471a5cdab5ae07e825173cf3a23675fed
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 17:25:03 -0400

    Saving colorCodedMap.R

--- colorCodedMap.R
+++ colorCodedMap.R
@@ -19,4 +19,4 @@ map.murderRate <- map.murderRate + coord_map() +  ggtitle("basic map of continen
  
  
 map.murderRate <- map.murderRate + geom_point(x = mergedData$StateCenterX, y= mergedData$StateCenterY, y = 40,
-color="white", size = mergedData$population)
+color="white", aes(size = mergedData$population))
