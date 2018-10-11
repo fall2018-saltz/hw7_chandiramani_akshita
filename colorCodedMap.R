db64b5a3f5518d5e71582dd59d05fe3f3a36e270
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 16:41:37 -0400

    Saving colorCodedMap.R

--- colorCodedMap.R
+++ colorCodedMap.R
@@ -7,6 +7,6 @@ head(mergedData)
  mergedData$StateName <- tolower(mergedData$StateName)
 map.simple <- ggplot(mergedData, aes(map_id = StateName))  
 map.simple <- map.simple+  geom_map(map = us, fill="white", color="black") 
-#map.simple <- map.simple + expand_limits(x = us$lat, y = us$long)
+map.simple <- map.simple + expand_limits(x = us$lat, y = us$long)
  #map.simple <- map.simple + coord_map() +  ggtitle("basic map of continental USA")
  
