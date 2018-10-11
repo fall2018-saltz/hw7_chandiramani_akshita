2896eb607c45257e82fd2b9fafbf11912875da8f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 16:22:56 -0400

    Saving colorCodedMap.R

--- colorCodedMap.R
+++ colorCodedMap.R
@@ -7,5 +7,5 @@ library("ggmap")
 head(mergedData)
 
 usa <- map_data("usa") # we already did this, but we can do it again
-ggplot() + geom_polygon(data = usa, aes(x=long, y = lat, group = group)) + 
+map.popColor<-ggplot() + geom_polygon(data = usa, aes(x=long, y = lat, group = group)) + 
   coord_fixed(1.3)
