bc2e94f142e65514367720fd712f40557fad2031
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 11:41:40 -0400

    Saving merge.R

--- merge.R
+++ merge.R
@@ -11,5 +11,5 @@ colnames(mergedData)[1] <- "StateName"
 mergedData <- mergedData[-c(9),]
 mergedData$StateName <- state.name
 mergedData$StateArea <- state.area
+mergedData$StateCenter <- state.center
 
-state.center
