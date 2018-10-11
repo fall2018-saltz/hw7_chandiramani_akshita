0a215c36cf5c9bd986f029f48475a0abff41d563
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 12:19:10 -0400

    Saving colorCodedMap.R

--- colorCodedMap.R
+++ colorCodedMap.R
@@ -4,6 +4,6 @@ library("ggmap")
 
 states <- map_data("state")
 colormap<-ggplot(data = states) + 
-  geom_polygon(aes(x = long, y = lat, fill = region, group = group), color = "white") + 
+  geom_polygon(aes(x = long, y = lat, fill = area, group = group), color = "white") + 
   coord_fixed(1.3) +
   guides(fill=FALSE)
