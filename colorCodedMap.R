4e0154850aa58ed1ce54acc637cc9bd9b1e8de4a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 16:18:15 -0400

    Saving colorCodedMap.R

--- colorCodedMap.R
+++ colorCodedMap.R
@@ -5,7 +5,7 @@ library("ggmap")
 
 
 head(mergedData)
-
+ us <- map_data("state")
  map.popColor <- ggplot(mergedData, aes(map_id = state))  
  map.popColor <- map.popColor + geom_map(map = us, aes(fill=StateArea)) 
  map.popColor <- map.popColor + expand_limits(x = us$long, y = us$lat)
