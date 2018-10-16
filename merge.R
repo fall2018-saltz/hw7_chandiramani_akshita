775ff548c62d4d8c7ae3e409a3bcf06f18e51d61
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-15 22:05:20 -0400

    Saving merge.R

--- merge.R
+++ merge.R
@@ -7,7 +7,7 @@ mergedData <- merge(arrests, cleanDfStates, by="row.names", all=TRUE)
 #and rename to StateName
 mergedData <- mergedData[,-c(6)]
 colnames(mergedData)[1] <- "StateName"
-
+state.center
 mergedData <- mergedData[-c(9),]
 mergedData$StateName <- state.name
 mergedData$StateArea <- state.area
