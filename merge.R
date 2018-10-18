c5652990d1d1ecefdbbf1b65e9539a378fc94936
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 22:48:22 -0400

    Saving merge.R

--- merge.R
+++ merge.R
@@ -8,6 +8,8 @@ mergedData <- merge(arrests, cleanDfStates, by="row.names", all=TRUE)
 mergedData <- mergedData[,-c(6)]
 colnames(mergedData)[1] <- "StateName"
 state.center
+
+#2)	Add the area of each state, and the center of each state, to the merged dataframe, using the ‘state.center’, ‘state.center’ and ‘state.name’ vectors
 mergedData <- mergedData[-c(9),]
 mergedData$StateName <- state.name
 mergedData$StateArea <- state.area
